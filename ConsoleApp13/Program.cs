9af8e9689de7725a0fb77af6d2c447b57ed8ccce
bella <theboss@ciaobella.me>
2021-06-18 13:46:53 -0700

    first commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car()
+            {
+                Make = "Ford",
+                Model = "focus",
+                Year = 2021
+            };
+
+            var mazda = new Car()
+
+            {
+                Make = "Mazdza",
+                Model = "CX-5",
+                Year = 2056,
+            };
+
+            var chevy = new Car("Chevy", "impala", 2001);
+
+            var carList = new List<Car>() { myCar, mazda, chevy, };
+            
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model}{vehicle.Year}");
+            }
+
+
+
+
+        }
+    }
+}
